94ba5326c2b6fa8d0e8166468943faed10b3a741
J.Kummer <jkummer@3pc.de>
2018-01-23 17:25:56 +0100

    [TASK] Adapt locallang for TYPO3 version 7 and 8

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,10 +33,10 @@ $EM_CONF[$_EXTKEY] = array(
     '_md5_values_when_last_written' => '',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.5.0-7.0.99',
-            'typo3' => '6.2.0-7.9.99',
+            'php' => '5.5.0-7.1.99',
+            'typo3' => '7.6.0-8.7.99',
             'oelib' => '0.9.52-1.9.99',
-            'ameos_formidable' => '1.1.564-1.9.99',
+            'ameos_formidable' => '1.1.564-2.1.99',
             'static_info_tables' => '6.2.0-',
         ),
         'conflicts' => array(
@@ -46,6 +46,20 @@ $EM_CONF[$_EXTKEY] = array(
             'sr_feuser_register' => '',
         ),
     ),
-    'suggests' => array(
-    ),
+    'autoload' => [
+        'classmap' => [
+            'Ajax',
+            'BackEnd',
+            'cli',
+            'lib',
+            'Mapper',
+            'Model',
+            'pi1'
+        ],
+    ],
+    'autoload-dev' => [
+        'classmap' => [
+            'tests',
+        ],
+    ],
 );
